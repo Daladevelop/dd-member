e6cbea489f68f3d692939eeaa4047c03c04d1bbf
Emil Österlund <emil@dalnix.se>
2018-03-07 13:43:20 +0100

    Small changes towards payments

--- app/Event.php
+++ app/Event.php
@@ -2,9 +2,10 @@
 
 namespace App;
 
+use App\Helpers\Traits\Payable;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    use Payable;
 }
--- app/Payment.php
+++ app/Payment.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    public function payable(){
+        return $this->morphTo();
+    }
 }
